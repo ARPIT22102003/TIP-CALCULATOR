c5fe7c191da49b34e134df3431edac807381db68
Arpit Singhal <arpitsinghal00001@gmail.com>
2023-10-18 07:31:40 +0530

    gui added

--- pythonproject1.py
+++ pythonproject1.py
@@ -1,48 +1,49 @@
-#tip calculator
-print("Enter the bill:")
+import tkinter as tk
 
+def calculate_tip():
+    bill = float(bill_entry.get())
+    occupation_type = occupation_var.get()
+    
+    if occupation_type == 'Restaurants' or occupation_type == 'Food delivery' or occupation_type == 'Hotel service':
+        tip_percent = 15
+    elif occupation_type == 'Driver' or occupation_type == 'Dressing Parlour':
+        tip_percent = 10
+    elif occupation_type == 'Home service':
+        tip_percent = 5
+    else:
+        tip_percent = 0
 
-bill = float(input())    #taking input of the total bill
+    tip = (tip_percent / 100) * bill
+    total_bill = bill + tip
+    
+    result_label.config(text=f"Bill: {bill}\nTip Percent: {tip_percent}%\nTotal Bill: {total_bill}")
 
-print("Enter the occupation type:")
+# Create the main window
+root = tk.Tk()
+root.title("Tip Calculator")
 
-occupation_type= str(input())  #taking input of the occupation to findout the tip percent to be charged
+# Bill entry
+bill_label = tk.Label(root, text="Enter the bill:")
+bill_label.pack()
+bill_entry = tk.Entry(root)
+bill_entry.pack()
 
-#types
-if(occupation_type == 'Restraunts' ):
-    tip_percent = 15
+# Occupation dropdown
+occupation_label = tk.Label(root, text="Select occupation type:")
+occupation_label.pack()
+occupation_var = tk.StringVar(root)
+occupation_var.set("Restaurants")  # Default value
+occupations = ['Restaurants', 'Food delivery', 'Hotel service', 'Driver', 'Dressing Parlour', 'Home service']
+occupation_dropdown = tk.OptionMenu(root, occupation_var, *occupations)
+occupation_dropdown.pack()
 
+# Calculate button
+calculate_button = tk.Button(root, text="Calculate Tip", command=calculate_tip)
+calculate_button.pack()
 
-elif(occupation_type == 'Food delivery'):
-    tip_percent = 15
+# Result label
+result_label = tk.Label(root, text="")
+result_label.pack()
 
-
-elif(occupation_type == 'Hotel service'):
-    tip_percent = 15
-
-
-elif(occupation_type == 'Driver'):
-    tip_percent = 10
-
-
-elif(occupation_type == 'Dressing Parlour'):
-    tip_percent = 10
-
-
-elif(occupation_type == 'Home service'):
-    tip_percent = 5
-
-
-else:
-    tip_percent = 0
-
-
-tip = (tip_percent/100)*bill
-
-total_bill = bill + tip
-
-print("Bill "+ str(bill))
-
-print("Tip Percent " + str(tip_percent))
-
-print("Total Bill "+str(total_bill))
+# Start the GUI event loop
+root.mainloop()
